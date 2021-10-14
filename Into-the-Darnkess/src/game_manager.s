3d5c9faa860e3fa840f7fb9db0fccbbb05b71297
JorgeMF10 <jmf62@gcloud.ua.es>
2021-10-14 19:07:14 +0200

    ya funciona la ia

--- Into-the-Darnkess/src/game_manager.s
+++ Into-the-Darnkess/src/game_manager.s
@@ -6,15 +6,13 @@
 
 
 ;;                      x   y   h   w    vx  vy   Color VMPtr   ltr IAState
-DefineEntity player,    50, 50, 2,  8,   1,  1, 0xF0, 0xC000,  0, e_ai_st_noAI
-DefineEntity enemy ,    40, 80, 4,  16,  1, -1, 0xFF, 0xC000,  0, e_ai_st_move_to
-DefineEntity bloc  ,    30, 30, 2,  8,   0,  0, 0x0F, 0xC000,  0, e_ai_st_noAI
-
-
+DefineEntity player,    50, 50, 2,  8,   0,  0, 0xF0, 0xC000,  0, e_ai_st_noAI
+DefineEntity enemy ,    40, 80, 4,  16, -1,  1, 0xFF, 0xC000,  0, e_ai_st_stand_by
+DefineEntity bloc  ,    30, 30, 4,  16,  0,  0, 0x0F, 0xC000,  0, e_ai_st_noAI
 
 man_game_init::
 
-    call man_entity_getArray
+    call entityman_getEntityVector_IX
     call sys_ai_init
     call man_entity_init
     call rendersys_init
@@ -36,7 +34,7 @@ man_game_update::
 
     call man_entity_getArray
     call sys_input_update
-    call entityman_getNumEntities_A
+    call man_entity_getArray
     call sys_ai_update
     call man_entity_getArray
     call sys_physics_update
--- Into-the-Darnkess/src/ia_manager.s
+++ Into-the-Darnkess/src/ia_manager.s
@@ -9,40 +9,78 @@ sys_ai_init::
 ret
 
 sys_ai_stand_by::
-_ent_array_ptr_temp_standby = . + 2
+
+    ;;ld e_vx(ix), #-1
+    ;;ld e_vy(ix), #-1
+
+_ent_array_ptr_temp_standby = .+2
     ld iy, #0x0000
 
-    ld  a, e_ai_aim_x(iy)
+    ld a, e_ai_aim_x(iy)
     or a
     ret z
-
-    ld a, e_x(ix)
+    
+    ;; MOVE TO PLAYER
+    ;; cargo posicion delplayer en target de enemy    
+    ld a, e_x(iy)
     ld e_ai_aim_x(ix), a
-    ld a, e_y(ix)
+    ld a, e_y(iy)
     ld e_ai_aim_y(ix), a
     ld e_ai_st(ix), #e_ai_st_move_to
 
 ret
 
 sys_ai_move_to::
-    
+
     ld e_vy(ix), #0
-    
-;;    ld a, e_ai_aim_x(ix) ;; A = x_obj
-;;    sub e_x(ix)
-;;    jr nc, _objx_greater_or_equal
-;;
-;;_objx_lesser:
-;;    ld e_vx(ix), #-1
-;;    jr _endif_x
-;;_objx_greater_or_equal:
-;;    jr z, _arrived_x
-;;    ld e_vx(ix), #1
-;;    jr _endif_x
-;;_arrived_x:
-;;    ld e_vx(ix), #0
-;;    ld e_ai_st(ix), #e_ai_st_stand_by 
-;;_endif_x:
+    ld a, e_ai_aim_x(ix) ;; A = x_obj
+    sub e_x(ix)
+    jr nc, _objx_greater_or_equal
+
+_objx_lesser:
+
+    ld e_vx(ix), #-1
+    jr _endif_x
+
+_objx_greater_or_equal:
+
+    jr z, _arrived_x
+    ld e_vx(ix), #1
+    jr _endif_x
+
+_arrived_x:
+
+    ld e_vx(ix), #0
+    ld e_ai_st(ix), #e_ai_st_stand_by 
+
+_endif_x:
+
+    ld a, e_ai_aim_y(ix) ;; A = y_obj
+    sub e_y(ix)
+    jr nc, _objy_greater_or_equal
+
+_objy_lesser:
+
+    ld e_vy(ix), #-1
+    jr _endif_y
+
+_objy_greater_or_equal:
+
+    jr z, _arrived_y
+    ld e_vy(ix), #1
+    jr _endif_y
+
+_arrived_y:
+
+    ld e_vy(ix), #0
+    ld a, e_vx(ix)
+    or a
+    jr nz, _endif_y
+
+        ld e_ai_st(ix), #e_ai_st_stand_by 
+
+_endif_y:
+
 
 ret
 
@@ -53,7 +91,7 @@ _ent_array_ptr = . + 2
     ld ix, #0x0000
 _loop: 
 
-    ld a, e_ai_st(ix)
+    ld a, e_ai_st(ix) ;;creo que aqui no esto cargando bien esto
     cp #e_ai_st_noAI
 
     jr z, _no_AI_ent
